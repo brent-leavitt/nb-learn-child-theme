6811dd1f9d30d9edccb5ff77a9f7ba831826ffe6
Brent Leavitt <brent@trainingdoulas.com>
2023-06-02 16:05:26 -0500

    minor changs to CSS and jquery files.

--- func/nbcbl.js
+++ func/nbcbl.js
@@ -1,9 +1,13 @@
 jQuery.noConflict();
 (function($) { 
 	$(document).ready(function(){
-		
-		$('.home-page-subtitle').append('<a id="home-page-subtitle-cta" class="kad-btn-primary kad-btn lg-kad-btn" href="/access/">Start for Free!</a> ');
-
+		const attr = $('body').hasClass( 'logged-in' ); 
+		if ( typeof attr == 'undefined' || attr == false )
+		{
+			$('.home-page-subtitle').append('<a id="home-page-subtitle-cta" class="kad-btn-primary kad-btn lg-kad-btn" href="/register/">Start Now!</a> ');
+		}
 		
 	});
 })(jQuery);
+
+//logged-in
